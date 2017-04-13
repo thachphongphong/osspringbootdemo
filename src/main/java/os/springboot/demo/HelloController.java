72aba7c129469c99a7e28587a6519339838542f8
Linh Do <thachphongphong@gmail.com>
2017-04-13 11:04:49 +0700

    Initial commit

---
+++ src/main/java/os/springboot/demo/HelloController.java
@@ -0,0 +1,17 @@
+package os.springboot.demo;
+
+/**
+ * Created by linhdo on 4/13/17.
+ */
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "Greetings from Spring Boot!";
+    }
+
+}
---
+++ src/main/java/os/springboot/demo/OsSpringbootDemoApplication.java
@@ -0,0 +1,12 @@
+package os.springboot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsSpringbootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OsSpringbootDemoApplication.class, args);
+	}
+}
